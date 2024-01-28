e0c3221615678392685469864ca4308bb604347d
Jörg Bayreuther <jb@visualjerk.de>
2024-01-28 18:41:47 +0100

    add locations

--- packages/scenarios/mystery-of-the-hogwarts-express/index.ts
+++ packages/scenarios/mystery-of-the-hogwarts-express/index.ts
@@ -1,5 +1,26 @@
 import { LocationCard, Scenario } from '@lumos/game'
 
+const Aisle2: LocationCard = {
+  id: 'aisle-2',
+  name: 'Aisle 2',
+  incomingConnection: 'moon',
+  connections: ['diamond', 'heart', 'circle'],
+}
+
+const Compartment24: LocationCard = {
+  id: 'compartment-24',
+  name: 'Compartment 24',
+  incomingConnection: 'diamond',
+  connections: ['moon'],
+}
+
+const Compartment25: LocationCard = {
+  id: 'compartment-25',
+  name: 'Compartment 25',
+  incomingConnection: 'heart',
+  connections: ['moon'],
+}
+
 const Aisle3: LocationCard = {
   id: 'aisle-3',
   name: 'Aisle 3',
@@ -29,9 +50,20 @@ const BoardRestaurant: LocationCard = {
 }
 
 export const MisteryOfTheHogwartsExpress: Scenario = {
-  locationCards: [Aisle3, Compartment34, Compartment35, BoardRestaurant],
+  locationCards: [
+    Aisle2,
+    Compartment24,
+    Compartment25,
+    Aisle3,
+    Compartment34,
+    Compartment35,
+    BoardRestaurant,
+  ],
   startLocation: Compartment34.id,
   layout: new Map([
+    [Aisle2.id, [2, 2]],
+    [Compartment24.id, [1, 3]],
+    [Compartment25.id, [2, 3]],
     [Aisle3.id, [4, 2]],
     [Compartment34.id, [3, 3]],
     [Compartment35.id, [4, 3]],
