bd645f691677d4e41bde037c29213139997d3371
Jörg Bayreuther <jb@visualjerk.de>
2024-02-06 22:48:40 +0100

    remove helper functions

--- packages/game/src/context.ts
+++ packages/game/src/context.ts
@@ -62,108 +62,240 @@ export class Context {
   ) {}
 
   getDoomCard(): DoomCard {
-    return getDoomCard(this)
+    return this.scenario.doomCards.find(
+      (card) => card.id === this.doomState.doomCardId
+    )!
   }
 
   getNextDoomCardId(): DoomCardId | undefined {
-    return getNextDoomCardId(this)
+    return this.getDoomCard().nextDoomCardId
   }
 
   getSceneCard(): SceneCard {
-    return getSceneCard(this)
+    return this.scenario.sceneCards.find(
+      (card) => card.id === this.sceneState.sceneCardId
+    )!
   }
 
   getTotalInvestigatorClues(): number {
-    return getTotalInvestigatorClues(this)
+    let totalClues = 0
+    this.investigatorStates.forEach(({ clues }) => {
+      totalClues += clues
+    })
+    return totalClues
   }
 
   getNextSceneCardId(): string | undefined {
-    return getNextSceneCardId(this)
+    return this.getSceneCard().nextSceneCardId
   }
 
   getLocationInvestigators(locationId: LocationId): Investigator[] {
-    return getLocationInvestigators(this, locationId)
+    const investigators: Investigator[] = []
+    this.investigatorStates.forEach((investigatorState, investigatorId) => {
+      if (investigatorState.currentLocation !== locationId) {
+        return
+      }
+      investigators.push(this.getInvestigator(investigatorId)!)
+    })
+
+    return investigators
   }
 
   getInvestigator(investigatorId: InvestigatorId) {
-    return getInvestigator(this, investigatorId)
+    return this.investigators.find(
+      (investigator) => investigator.id === investigatorId
+    )!
   }
 
   canDrawFromDeck(investigatorId: InvestigatorId) {
-    return canDrawFromDeck(this, investigatorId)
+    const investigatorState = this.investigatorStates.get(investigatorId)
+
+    if (!investigatorState) {
+      throw new Error('Investigator not found')
+    }
+
+    return canDraw(investigatorState)
   }
 
   drawFromDeck(investigatorId: InvestigatorId): Context {
-    return drawFromDeck(this, investigatorId)
+    const investigatorState = this.investigatorStates.get(investigatorId)
+
+    if (!investigatorState) {
+      throw new Error('Investigator not found')
+    }
+
+    this.investigatorStates.set(investigatorId, draw(investigatorState))
+
+    return this
   }
 
   discardFromHand(investigatorId: InvestigatorId, cardIndex: number): Context {
-    return discardFromHand(this, investigatorId, cardIndex)
+    const investigatorState = this.investigatorStates.get(investigatorId)
+
+    if (!investigatorState) {
+      throw new Error('Investigator not found')
+    }
+
+    this.investigatorStates.set(
+      investigatorId,
+      discard(investigatorState, cardIndex)
+    )
+
+    return this
   }
 
   removeCardFromHand(
     investigatorId: InvestigatorId,
     cardIndex: number
   ): Context {
-    return removeCardFromHand(this, investigatorId, cardIndex)
+    const investigatorState = this.investigatorStates.get(investigatorId)
+
+    if (!investigatorState) {
+      throw new Error('Investigator not found')
+    }
+
+    this.investigatorStates.set(
+      investigatorId,
+      removeFromHand(investigatorState, cardIndex)
+    )
+
+    return this
   }
 
   addCardToDiscardPile(
     investigatorId: InvestigatorId,
     cardId: string
   ): Context {
-    return addCardToDiscardPile(this, investigatorId, cardId)
+    const investigatorState = this.investigatorStates.get(investigatorId)
+
+    if (!investigatorState) {
+      throw new Error('Investigator not found')
+    }
+
+    this.investigatorStates.set(
+      investigatorId,
+      addToDiscardPile(investigatorState, cardId)
+    )
+
+    return this
   }
 
   playCardFromHand(investigatorId: InvestigatorId, cardIndex: number): Context {
-    return playCardFromHand(this, investigatorId, cardIndex)
+    const investigatorState = this.investigatorStates.get(investigatorId)
+
+    if (!investigatorState) {
+      throw new Error('Investigator not found')
+    }
+
+    this.investigatorStates.set(
+      investigatorId,
+      play(investigatorState, cardIndex)
+    )
+
+    return this
   }
 
   getEncounterCard(encounterCardId: string) {
-    return getEncounterCard(this, encounterCardId)
+    return this.scenario.encounterCards.find(
+      (card) => card.id === encounterCardId
+    )!
   }
 
   drawEncounterCard(): Context {
-    return drawEncounterCard(this)
+    this.encounterState = drawEncounter(this.encounterState)
+    return this
   }
 
   discardCurrentEncounterCard(): Context {
-    return discardCurrentEncounterCard(this)
+    this.encounterState = discardCurrentEncounter(this.encounterState)
+    return this
   }
 
   moveInvestigator(
     investigatorId: InvestigatorId,
     locationId: LocationId
   ): Context {
-    return moveInvestigator(this, investigatorId, locationId)
+    const investigatorState = this.investigatorStates.get(investigatorId)
+    const locationState = this.locationStates.get(locationId)
+
+    if (!investigatorState) {
+      throw new Error('Investigator not found')
+    }
+
+    if (!locationState) {
+      throw new Error('Location not found')
+    }
+
+    investigatorState.currentLocation = locationId
+    locationState.revealed = true
+
+    return this
   }
 
   getLocation(locationId: LocationId): LocationCard {
-    return getLocation(this, locationId)
+    return this.scenario.locationCards.find(
+      (location) => location.id === locationId
+    )!
   }
 
   getInvestigatorLocation(investigatorId: InvestigatorId): LocationCard {
-    return getInvestigatorLocation(this, investigatorId)
+    const investigator = this.investigatorStates.get(investigatorId)
+
+    if (!investigator) {
+      throw new Error('Investigator not found')
+    }
+
+    const location = this.getLocation(investigator.currentLocation)
+
+    if (!location) {
+      throw new Error('Location not found')
+    }
+
+    return location
   }
 
   collectClue(investigatorId: InvestigatorId, locationId: LocationId): Context {
-    return collectClue(this, investigatorId, locationId)
+    const investigator = this.investigatorStates.get(investigatorId)
+    const location = this.locationStates.get(locationId)
+
+    if (!investigator) {
+      throw new Error('Investigator not found')
+    }
+
+    if (!location) {
+      throw new Error('Location not found')
+    }
+
+    if (!location.revealed) {
+      throw new Error('Location not revealed')
+    }
+
+    if (location.clues === 0) {
+      throw new Error('Location has no clues')
+    }
+
+    location.clues--
+    investigator.clues++
+
+    return this
   }
 
   getInvestigatorState(investigatorId: InvestigatorId): InvestigatorState {
-    return getInvestigatorState(this, investigatorId)
+    return this.investigatorStates.get(investigatorId)!
   }
 
   getInvestigatorCardsInHand(
     investigatorId: InvestigatorId
   ): InvestigatorCard[] {
-    return getInvestigatorCardsInHand(this, investigatorId)
+    const investigatorState = this.getInvestigatorState(investigatorId)
+    return investigatorState.cardsInHand.map(getInvestigatorCard)
   }
 
   getInvestigatorCardsInPlay(
     investigatorId: InvestigatorId
   ): InvestigatorCard[] {
-    return getInvestigatorCardsInPlay(this, investigatorId)
+    const investigatorState = this.getInvestigatorState(investigatorId)
+    return investigatorState.cardsInPlay.map(getInvestigatorCard)
   }
 
   getInvestigatorCard(cardId: string): InvestigatorCard {
@@ -171,300 +303,21 @@ export class Context {
   }
 
   getInvestigatorSkills(investigatorId: InvestigatorId): Skills {
-    return getInvestigatorSkills(this, investigatorId)
+    const investigator = this.getInvestigator(investigatorId)
+    const cards = this.getInvestigatorCardsInPlay(investigatorId)
+
+    const skills = { ...investigator.baseSkills }
+    Object.keys(skills).forEach((skill) => {
+      cards.forEach((card) => {
+        skills[skill as keyof Skills] +=
+          card.permanentSkillModifier?.[skill as keyof Skills] ?? 0
+      })
+    })
+
+    return skills
   }
-}
-
-function getDoomCard(context: Context): DoomCard {
-  return context.scenario.doomCards.find(
-    (card) => card.id === context.doomState.doomCardId
-  )!
-}
-
-function getNextDoomCardId(context: Context): DoomCardId | undefined {
-  return getDoomCard(context).nextDoomCardId
-}
-
-function getSceneCard(context: Context): SceneCard {
-  return context.scenario.sceneCards.find(
-    (card) => card.id === context.sceneState.sceneCardId
-  )!
-}
-
-function getTotalInvestigatorClues(context: Context): number {
-  let totalClues = 0
-  context.investigatorStates.forEach(({ clues }) => {
-    totalClues += clues
-  })
-  return totalClues
-}
-
-function getNextSceneCardId(context: Context): string | undefined {
-  return getSceneCard(context).nextSceneCardId
-}
-
-function getLocationInvestigators(
-  context: Context,
-  locationId: LocationId
-): Investigator[] {
-  const investigators: Investigator[] = []
-  context.investigatorStates.forEach((investigatorState, investigatorId) => {
-    if (investigatorState.currentLocation !== locationId) {
-      return
-    }
-    investigators.push(getInvestigator(context, investigatorId)!)
-  })
-
-  return investigators
-}
-
-function getInvestigator(context: Context, investigatorId: InvestigatorId) {
-  return context.investigators.find(
-    (investigator) => investigator.id === investigatorId
-  )!
-}
-
-function canDrawFromDeck(context: Context, investigatorId: InvestigatorId) {
-  const investigatorState = context.investigatorStates.get(investigatorId)
-
-  if (!investigatorState) {
-    throw new Error('Investigator not found')
-  }
-
-  return canDraw(investigatorState)
-}
-
-function drawFromDeck(
-  context: Context,
-  investigatorId: InvestigatorId
-): Context {
-  const investigatorState = context.investigatorStates.get(investigatorId)
-
-  if (!investigatorState) {
-    throw new Error('Investigator not found')
-  }
-
-  context.investigatorStates.set(investigatorId, draw(investigatorState))
-
-  return context
-}
-
-function discardFromHand(
-  context: Context,
-  investigatorId: InvestigatorId,
-  cardIndex: number
-): Context {
-  const investigatorState = context.investigatorStates.get(investigatorId)
-
-  if (!investigatorState) {
-    throw new Error('Investigator not found')
-  }
-
-  context.investigatorStates.set(
-    investigatorId,
-    discard(investigatorState, cardIndex)
-  )
-
-  return context
-}
-
-function removeCardFromHand(
-  context: Context,
-  investigatorId: InvestigatorId,
-  cardIndex: number
-): Context {
-  const investigatorState = context.investigatorStates.get(investigatorId)
-
-  if (!investigatorState) {
-    throw new Error('Investigator not found')
-  }
-
-  context.investigatorStates.set(
-    investigatorId,
-    removeFromHand(investigatorState, cardIndex)
-  )
-
-  return context
-}
-
-function addCardToDiscardPile(
-  context: Context,
-  investigatorId: InvestigatorId,
-  cardId: string
-): Context {
-  const investigatorState = context.investigatorStates.get(investigatorId)
-
-  if (!investigatorState) {
-    throw new Error('Investigator not found')
-  }
-
-  context.investigatorStates.set(
-    investigatorId,
-    addToDiscardPile(investigatorState, cardId)
-  )
-
-  return context
-}
-
-function playCardFromHand(
-  context: Context,
-  investigatorId: InvestigatorId,
-  cardIndex: number
-): Context {
-  const investigatorState = context.investigatorStates.get(investigatorId)
-
-  if (!investigatorState) {
-    throw new Error('Investigator not found')
-  }
-
-  context.investigatorStates.set(
-    investigatorId,
-    play(investigatorState, cardIndex)
-  )
-
-  return context
-}
-
-function getEncounterCard(context: Context, encounterCardId: string) {
-  return context.scenario.encounterCards.find(
-    (card) => card.id === encounterCardId
-  )!
-}
-
-function drawEncounterCard(context: Context): Context {
-  context.encounterState = drawEncounter(context.encounterState)
-  return context
-}
-
-function discardCurrentEncounterCard(context: Context): Context {
-  context.encounterState = discardCurrentEncounter(context.encounterState)
-  return context
-}
-
-function moveInvestigator(
-  context: Context,
-  investigatorId: InvestigatorId,
-  locationId: LocationId
-): Context {
-  const investigatorState = context.investigatorStates.get(investigatorId)
-  const locationState = context.locationStates.get(locationId)
-
-  if (!investigatorState) {
-    throw new Error('Investigator not found')
-  }
-
-  if (!locationState) {
-    throw new Error('Location not found')
-  }
-
-  investigatorState.currentLocation = locationId
-  locationState.revealed = true
-
-  return context
-}
-
-function getLocation(context: Context, locationId: LocationId) {
-  return context.scenario.locationCards.find(
-    (location) => location.id === locationId
-  )!
-}
-
-function getInvestigatorLocation(
-  context: Context,
-  investigatorId: InvestigatorId
-) {
-  const investigator = context.investigatorStates.get(investigatorId)
-
-  if (!investigator) {
-    throw new Error('Investigator not found')
-  }
-
-  const location = context.getLocation(investigator.currentLocation)
-
-  if (!location) {
-    throw new Error('Location not found')
-  }
-
-  return location
-}
-
-function collectClue(
-  context: Context,
-  investigatorId: InvestigatorId,
-  locationId: LocationId
-): Context {
-  const investigator = context.investigatorStates.get(investigatorId)
-  const location = context.locationStates.get(locationId)
-
-  if (!investigator) {
-    throw new Error('Investigator not found')
-  }
-
-  if (!location) {
-    throw new Error('Location not found')
-  }
-
-  if (!location.revealed) {
-    throw new Error('Location not revealed')
-  }
-
-  if (location.clues === 0) {
-    throw new Error('Location has no clues')
-  }
-
-  location.clues--
-  investigator.clues++
-
-  return context
-}
-
-function getInvestigatorState(
-  context: Context,
-  investigatorId: InvestigatorId
-) {
-  return context.investigatorStates.get(investigatorId)!
-}
-
-function getInvestigatorCardsInHand(
-  context: Context,
-  investigatorId: InvestigatorId
-) {
-  const investigatorState = context.getInvestigatorState(investigatorId)
-
-  return investigatorState.cardsInHand.map(getInvestigatorCard)
-}
-
-function getInvestigatorCardsInPlay(
-  context: Context,
-  investigatorId: InvestigatorId
-) {
-  const investigatorState = context.getInvestigatorState(investigatorId)
-
-  return investigatorState.cardsInPlay.map(getInvestigatorCard)
 }
 
 export function getInvestigatorCard(cardId: string) {
   return InvestigatorCardCollection.get(cardId)!
-}
-
-function getLocationState(context: Context, locationId: LocationId) {
-  return context.locationStates.get(locationId)!
-}
-
-function getInvestigatorSkills(
-  context: Context,
-  investigatorId: InvestigatorId
-): Skills {
-  const investigator = getInvestigator(context, investigatorId)
-  const cards = getInvestigatorCardsInPlay(context, investigatorId)
-
-  const skills = { ...investigator.baseSkills }
-  Object.keys(skills).forEach((skill) => {
-    cards.forEach((card) => {
-      skills[skill as keyof Skills] +=
-        card.permanentSkillModifier?.[skill as keyof Skills] ?? 0
-    })
-  })
-
-  return skills
 }
