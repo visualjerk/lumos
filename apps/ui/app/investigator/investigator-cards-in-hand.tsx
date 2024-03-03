0d54ca6695110452d31e4d33e929031c1af723f5
Jörg Bayreuther <jb@visualjerk.de>
2024-03-03 09:43:08 +0100

    fix card positioning

--- apps/ui/app/investigator/investigator-cards-in-hand.tsx
+++ apps/ui/app/investigator/investigator-cards-in-hand.tsx
@@ -15,24 +15,6 @@ export default function InvestigatorCardsInHand({
   const state = context.getInvestigatorState(investigator.id)
   const { cardsInHand } = state
 
-  let center = cardsInHand.length / 2
-
-  if (center % 2 === 0) {
-    center += 0.5
-  }
-
-  function cardStyle(index: number) {
-    const position = index + 1
-
-    // TODO: fix positioning
-    const rotation = (position - center) * 2
-    const translateY = Math.abs((position - center) * 6)
-
-    return {
-      transform: `rotate(${rotation}deg) translateY(${translateY}px)`,
-    }
-  }
-
   return (
     <div
       className="grid w-96"
@@ -41,7 +23,7 @@ export default function InvestigatorCardsInHand({
       }}
     >
       {cardsInHand.map((id, index) => (
-        <div key={index} className="rotate-3" style={cardStyle(index)}>
+        <div key={index} style={cardStyle(index, cardsInHand.length)}>
           <InvestigatorCard
             key={index}
             id={id}
@@ -52,4 +34,17 @@ export default function InvestigatorCardsInHand({
       ))}
     </div>
   )
+}
+
+function cardStyle(index: number, totalCards: number) {
+  const center = (totalCards + 1) / 2
+  const position = index + 1
+  const diff = position - center
+
+  const rotation = diff * 3
+  const translateY = Math.abs(diff * 0.3)
+
+  return {
+    transform: `rotate(${rotation}deg) translateY(${translateY}rem)`,
+  }
 }
