63bee74e35ad2a391cb3ab0bc0f1e9a172111492
Jörg Bayreuther <jb@visualjerk.de>
2024-03-13 16:29:51 +0100

    remove unused imports with eslint

--- packages/eslint-config/base.cjs
+++ packages/eslint-config/base.cjs
@@ -2,9 +2,22 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'unused-imports'],
   ignorePatterns: ['node_modules', 'dist', '.eslintrc.js'],
   root: true,
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
   overrides: [
     {
       files: ['*.test.ts'],
