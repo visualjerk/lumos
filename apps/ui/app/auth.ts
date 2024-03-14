23efb9457262c6cdfda02fe7b8696807e89b7631
Jörg Bayreuther <jb@visualjerk.de>
2024-03-14 10:06:42 +0100

    memoize isAuthenticated

--- apps/ui/app/auth.ts
+++ apps/ui/app/auth.ts
@@ -1,6 +1,7 @@
 'use client'
 
 import { PlayerId } from '@lumos/game-server'
+import { useMemo } from 'react'
 import { useLocalStorage } from 'react-use'
 
 const PLAYER_ID_KEY = 'Lumos.PlayerId'
@@ -8,8 +9,10 @@ const PLAYER_ID_KEY = 'Lumos.PlayerId'
 export function useAuth() {
   const [playerId, setPlayerId] = useLocalStorage<PlayerId>(PLAYER_ID_KEY)
 
+  const isAuthenticated = useMemo(() => !!playerId, [playerId])
+
   return {
-    isAuthenticated: !!playerId,
+    isAuthenticated,
     setPlayerId,
     playerId,
   }
